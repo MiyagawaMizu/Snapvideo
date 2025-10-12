7091ce4dca81d3b62a8e4f37e1553089fd95f237
Mizu <71164002+MiyagawaMizu@users.noreply.github.com>
2025-10-12 16:57:39 +0700

    Remove FFmpeg-Builds-latest directory and all contents
    Deleted the entire FFmpeg-Builds-latest directory, including build scripts, Dockerfiles, CI workflows, utility scripts, patches, and documentation. This removes all build infrastructure and related files for FFmpeg static auto-builds from the repository.

--- FFmpeg-Builds-latest/util/dl_functions.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-default_dl() {
-    echo "git-mini-clone \"$SCRIPT_REPO\" \"$SCRIPT_COMMIT\" \"$1\""
-}
-
-ffbuild_dockerdl() {
-    default_dl .
-}
--- FFmpeg-Builds-latest/util/get_dl_cache_tag.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-set -eo pipefail
-cd "$(dirname "$0")"
-../download.sh hashonly | sha256sum | cut -d" " -f1
